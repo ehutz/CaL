fe25070d9719f29efed550c8c6311c99d68e862b
ehutz <ehutz@vt.edu>
2018-04-30 16:33:38 -0400

    , to :

--- mongoHelper.py
+++ mongoHelper.py
@@ -4,14 +4,15 @@ from pprint import pprint
 #checks if a username exists and returns the boolean
 def userExists(conn, username):
     db = conn.CaL
-    if db.CaL.find({'username' : username}).count() > 0:
+    if db.Users.find({'username' : username}).count() > 0:
+        #print(username)
         return True
     return False
 
 #checks if a username exists and returns the boolean
 def sessionExists(conn, session):
     db = conn.CaL
-    if db.CaL.find({'session' : session}).count() > 0:
+    if db.Session.find({'session' : session}).count() > 0:
         return True
     return False
 
@@ -20,7 +21,7 @@ def sessionExists(conn, session):
 #it is not attempting to overwrite an exxisting user
 def addUser(conn, username, password):
     db = conn.CaL
-    print(db.Users.count())
+    #print(db.Users.count())
     if userExists(conn, username):
         return False
     db.Users.insert({"username":username, "password":password})
@@ -30,10 +31,11 @@ def addUser(conn, username, password):
 #it is not attempting to overwrite an existing session
 def addSession(conn, session):
     db = conn.CaL
-    print(db.Session.count())
     if sessionExists(conn, session):
+        #print(db.Session.count())
         return False
     db.Session.insert({"session":session, "audio":None})
+    #print(db.Session.count())
     return True
 
 #adds audio to an existing session. Returns true if successful.
@@ -48,45 +50,84 @@ def addSessionAudio(conn, session, audio):
 #given a list of usernames, the session, the timestamp, and the (optional) image
 #the timestamp is listed in the individual users' tables,
 #and the timestamp and image is stored int he session's table
+'''
 def addTimestamp(conn, usernames, session, timestamp, image):
     db = conn.CaL
     #update the session's table
     sess_collection  = db[session]
-    if(image == None)
-        sess_collection.insert({'timestamp':timestamp, 'image', None})
-    else
+    if(image is None):
+        sess_collection.insert({'timestamp':timestamp, 'image': None})
+    else:
         #inserts the image for the current timestamp
-        sess_collection.insert({'timestamp':timestamp, 'image', image})
+        sess_collection.insert({'timestamp':timestamp, 'image': image})
         #add image to timestamp
     #update user collections
-    for username in usernames
+    for username in usernames:
         if userExists(conn, username):
             pid_collection = db[username]
+            print('Username' + username)
             if pid_collection.find({'session' : session}).count() > 0:
+                print('IF: new collection')
                 #update existing list
                 curr_stamps = pid_collection.find_one({'session' : session}).timestamps
                 curr_stamps = list(set(curr_stamps.append(timestamp)))
                 print(",".join(curr_stamps))
-                pid_collection.update({'session' : session}, {'timestamps', curr_stamps})
+                pid_collection.update({'session' : session}, {'timestamps': curr_stamps})
             else:
+                print('ELSE: new collection')
                 pid_collection.insert({'session' : session, 'timestamps' : [timestamp]})
-
+'''
+def addTimestamp(conn, username, session, timestamp, image):
+    #print(timestamp)
+    db = conn.CaL
+    #update the session's table
+    sess_collection  = db[session]
+    if(image is None):
+        sess_collection.insert({'timestamp':timestamp, 'image': None})
+    else:
+        #inserts the image for the current timestamp
+        sess_collection.insert({'timestamp':timestamp, 'image': image})
+        #add image to timestamp
+    #update user collections
+    if userExists(conn, username):
+        pid_collection = db[username]
+        #print('Username: ' + username)
+        if pid_collection.find({'session' : session}).count() > 0:
+            #print('IF: new collection')
+            #update existing list
+            curr_stamps_1 = pid_collection.find_one({'session' : session})
+            #pprint(curr_stamps_1)
+            curr_stamps = curr_stamps_1['timestamps']
+            #print(curr_stamps)
+            curr_stamps = curr_stamps + "," + timestamp #list(set(curr_stamps.append(timestamp)))
+            #print(curr_stamps)
+            #print(",".join(curr_stamps))
+            
+            pid_collection.update({'session' : session}, {'timestamps' : curr_stamps})
+        else:
+            #print('ELSE: new collection')
+            pid_collection.insert({'session' : session, 'timestamps' : timestamp})
+                
 #returns the list of usernames
 def getUsernames(conn):
     db = conn.CaL
     #print(db.Users.count())
     cursor = db.Users.find({})
     uList = []
-    for c in cursor: uList.append(c.username)
+    for c in cursor: uList.append(c["username"])
     return uList
 
 #returns the list of sessions and their audio in a list of tuples
 def getSessions(conn):
     db = conn.CaL
     #print(db.Sessions.count())
-    cursor = db.Session.find({})
+    #cursor = db.Session.find({})
     sList = []
-    for c in cursor: sList.append((c.username, c.audio))
+    #for c in cursor:
+    for c in db.Session.find():
+        if c is not None:
+            #sList.append((c.session, c.audio))
+            sList.append(c)
     return sList
 
 
