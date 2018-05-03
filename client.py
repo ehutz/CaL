975111a0e3b60f348356892efca4fd40af0659c1
Madelyn Newcomb <m1newc@vt.edu>
2018-05-02 22:05:36 -0400

    retrieving file and saving

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ import time
 from time import sleep
 from flask import Flask, request
 import requests
-
+from pprint import pprint
 # START: Parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", help="TCP_IP")
@@ -47,7 +47,13 @@ while True:
     status = requests.get('http://'+host_ip+':20000/status')
     print(status.text)
     if status.text == 'SESSION COMPLETE':
-        audio_file = requests.get('http://'+host_ip+':20000/audio/retrieve_file')
-       	break
+        audio = requests.get('http://'+host_ip+':20000/audio/retrieve_file')
+        audio_filename = (audio.headers['content-disposition']).split('filename=')[-1]
+        #audio_file = audio.file[audio_filename]
+        with open('./'+audio_filename, 'wb') as f:
+            for chunk in audio.iter_content(chunk_size=512 * 1024):
+                f.write(chunk)
+            f.close() 
+        break
     
             
